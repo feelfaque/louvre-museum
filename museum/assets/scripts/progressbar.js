71d05b012c2cc0cc060a176c91e5d8913b0c3afe
Anna Saenko <61340209+feelfaque@users.noreply.github.com>
2021-07-30 12:26:56 +0300

    Delete museum directory

--- museum/assets/scripts/progressbar.js
+++
@@ -1,7 +0,0 @@
-const progress = document.querySelectorAll('.range');
-  
-progress.forEach(el => el.addEventListener('input', function() {
-    const value = this.value;
-    this.style.background = `linear-gradient(to right, #24809E 0%, #24809E ${value}%, #C4C4C4 ${value}%, #C4C4C4 100%)`;
-  })
-  );
